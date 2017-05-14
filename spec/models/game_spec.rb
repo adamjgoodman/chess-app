e224a32b9575e373ad3e38334a96612cf975e8d3
Issei Horie <horie7188@gmail.com>
2017-05-14 04:41:19 +0000

    fix typo

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  it "should returns available games" do
+  it "should return available games" do
     FactoryGirl.create(:available_game)
     FactoryGirl.create(:completed_game)
     available_games = Game.available
