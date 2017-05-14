5a1a1c2d6e773d63c53d774076e85750899a2c52
Issei Horie <horie7188@gmail.com>
2017-05-14 04:43:52 +0000

    update test case

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   it "should return available games" do
     FactoryGirl.create(:available_game)
+    FactoryGirl.create(:available_game)
     FactoryGirl.create(:completed_game)
     available_games = Game.available
-    expect(available_games.size).to eq 1
+    expect(available_games.size).to eq 2
   end
 end
