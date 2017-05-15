c99532d2f30c918112465319f7d03c471df725e5
Issei Horie <horie7188@gmail.com>
2017-05-15 16:14:38 +0000

    listing available games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ApplicationRecord
+  belongs_to :user_black, class_name: 'User', foreign_key: 'user_id_black', optional: true
+  belongs_to :user_white, class_name: 'User', foreign_key: 'user_id_white', optional: true
+
   scope :available, (-> { where(status: 'available') })
 end
