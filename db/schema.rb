6c11797764e74c8959f9065d88153180332c06fc
Tyna Huynh <tynahuynh@gmail.com>
2017-05-16 01:15:10 +0000

    resolved merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511001336) do
+ActiveRecord::Schema.define(version: 20170511154200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.boolean  "active"
+    t.string   "status"
+    t.integer  "user_id_black"
+    t.integer  "user_id_white"
+    t.integer  "winner"
+    t.datetime "game_completed"
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.string   "type"
+    t.boolean  "black"
+    t.boolean  "white"
+    t.integer  "x_position"
+    t.integer  "y_position"
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
+    t.index ["user_id"], name: "index_pieces_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,10 +54,6 @@ ActiveRecord::Schema.define(version: 20170511001336) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.text     "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
