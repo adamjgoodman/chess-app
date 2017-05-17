428558d11bb8efaba3aa9dbe2ec4af49f9221ac0
Issei Horie <horie7188@gmail.com>
2017-05-17 13:58:26 +0000

    clean view

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   belongs_to :user_white, class_name: 'User', foreign_key: 'user_id_white', optional: true
 
   scope :available, (-> { where(status: 'available') })
+
+  def missing_player_color
+    self.user_id_black != nil ? "White" : "Black"
+  end
+
+  def host_player
+    self.user_black || self.user_white
+  end
 end
