aa58dae101a9f447ff65e41932dbd074a0912186
PunkLeia <theatrejawa@yahoo.com>
2017-05-24 21:37:47 +0000

    added tests for king moves

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,16 +3,15 @@ class King < Piece
   def move_valid?(x, y)
     while
       space_occupied?(x, y) == false # I think I can pull this straight from pieces
-      legal_moves(x, y) == true
-      check? == false
+      legal_move?(x, y) == true
+      check?(x, y) == false
     end
 
   end
 
-private
 
-  def legal_moves(x, y)
-    case position
+  def legal_move?(x, y)
+    case
     when
     (x_position - x).abs == 1 && y_position == y
       return true
@@ -28,9 +27,11 @@ private
   end
 
   # King is not allowed to put himself in check.  This retrieves the position of the opposing king
-  def check?(x, y)
-    White = Self.find_by_color("white")
-    Black = Self.find_by_color("black")
-  end
+  # def check?(x, y)
+  #   White = Self.find_by_color("white")
+  #   Black = Self.find_by_color("black")
+
+  # end
 
 end
+
