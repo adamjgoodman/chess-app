206ce99df69c83a2715584fe9454716e2f0435f3
PunkLeia <theatrejawa@yahoo.com>
2017-05-24 19:31:52 +0000

    added more king logic, created empty test file

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,5 +1,36 @@
 class King < Piece
-  def move_valid?
-    # add logic here
+
+  def move_valid?(x, y)
+    while
+      space_occupied?(x, y) == false # I think I can pull this straight from pieces
+      legal_moves(x, y) == true
+      check? == false
+    end
+
   end
+
+private
+
+  def legal_moves(x, y)
+    case position
+    when
+    (x_position - x).abs == 1 && y_position == y
+      return true
+    when
+    x_position == x && (y_position - y).abs == 1
+      return true
+    when
+    (x_position - x).abs == 1 && (y_position - y).abs == 1
+      return true
+    else
+      return false
+    end
+  end
+
+  # King is not allowed to put himself in check.  This retrieves the position of the opposing king
+  def check?(x, y)
+    White = Self.find_by_color("white")
+    Black = Self.find_by_color("black")
+  end
+
 end
