7ac260d6d05b3112f8d3a52af81647c7050a1e1d
PunkLeia <theatrejawa@yahoo.com>
2017-05-25 01:12:53 +0000

    refactored move logic

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,14 +1,14 @@
 class King < Piece
   def move_valid?(x, y)
-   if space_occupied?(x, y) == false && legal_move?(x, y) == true
-      # && check?(x, y) == false <when we have this logic
-    end
+    return false if space_occupied?(x, y) == true
+    return false if legal_move?(x, y) == false
+    return true
   end
 
   def legal_move?(x, y)
-    return true if (x_position - x).abs == 1 && y_position == y
-    return true if x_position == x && (y_position - y).abs == 1
-    return true if (x_position - x).abs == 1 && (y_position - y).abs == 1
-    else return false
+    return false if (x_position - x).abs != 1 && y_position == y
+    return false if x_position == x && (y_position - y).abs != 1
+    return false if (x_position - x).abs != 1 && (y_position - y).abs != 1
+    return true
   end
 end
