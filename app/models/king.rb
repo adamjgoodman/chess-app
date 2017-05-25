e7f39b34c43100c376736a518bb69ee971f34d67
PunkLeia <theatrejawa@yahoo.com>
2017-05-25 00:38:35 +0000

    roll back 'guard attempt'

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,15 +1,24 @@
 class King < Piece
   def move_valid?(x, y)
-    if space_occupied?(x, y) == false && legal_move?(x, y) == true
+   if space_occupied?(x, y) == false && legal_move?(x, y) == true
       # && check?(x, y) == false <when we have this logic
     end
   end
 
   def legal_move?(x, y)
-    return false unless
-      (x_position - x).abs == 1 && y_position == y
-      x_position == x && (y_position - y).abs == 1
-      (x_position - x).abs == 1 && (y_position - y).abs == 1
+    case legal
+    when
+    (x_position - x).abs == 1 && y_position == y
+      true
+    when
+    x_position == x && (y_position - y).abs == 1
+      true
+    when
+    (x_position - x).abs == 1 && (y_position - y).abs == 1
+      true
+    else
+      false
     end
+    return legal
   end
 end
