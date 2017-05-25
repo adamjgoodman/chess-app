ce500ee6f4ef03f6a0af73e3b55a603fdfac75a5
PunkLeia <theatrejawa@yahoo.com>
2017-05-25 01:23:51 +0000

    continued refactoring

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,13 +2,13 @@ class King < Piece
   def move_valid?(x, y)
     return false if space_occupied?(x, y) == true
     return false if legal_move?(x, y) == false
-    return true
+    true
   end
 
   def legal_move?(x, y)
     return false if (x_position - x).abs != 1 && y_position == y
     return false if x_position == x && (y_position - y).abs != 1
     return false if (x_position - x).abs != 1 && (y_position - y).abs != 1
-    return true
+    true
   end
 end
