f1a1938277922ccddd42e0dbef23a9d04ff88eed
PunkLeia <theatrejawa@yahoo.com>
2017-05-25 02:21:20 +0000

    pulled apart legal_move? method

--- app/models/king.rb
+++ app/models/king.rb
@@ -5,10 +5,25 @@ class King < Piece
     true
   end
 
+  def diagonal_legal?(x, y)
+    return (x_position - x).abs == 1 && (y_position - y).abs == 1 if diagonal_move?(x, y)
+    false
+  end
+
+  def horizontal_legal?(x, y)
+    return (x_position - x).abs == 1 && y_position == y if horizontal_move?(x, y)
+    false
+  end
+
+  def vertical_legal?(x, y)
+    return x_position == x && (y_position - y).abs == 1 if vertical_move?(x, y)
+    false
+  end
+
   def legal_move?(x, y)
-    return false if (x_position - x).abs != 1 && y_position == y
-    return false if x_position == x && (y_position - y).abs != 1
-    return false if (x_position - x).abs != 1 && (y_position - y).abs != 1
-    true
+    return true if diagonal_legal?(x, y)
+    return true if horizontal_legal?(x, y)
+    return true if vertical_legal?(x, y)
+    false
   end
 end
