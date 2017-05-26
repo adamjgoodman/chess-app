4d47e3a328d7e202da8c5052192060938e447809
DrKalen <kalen7@gmail.com>
2017-05-26 15:17:02 +0000

    Changed spec/games_controller and log in and sign up views adding username field

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe GamesController, type: :controller do
     it 'should successfully create a new game in our database' do
       post :create, params: { game: { user_id_black: 15 } }
       @game = Game.last
-      expect(response).to redirect_to game_path[:@game.id]
+      expect(response).to redirect_to game_path(@game.id)
       expect(game.user_id_black).to eq(15)
     end
   end
