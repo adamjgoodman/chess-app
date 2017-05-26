e1b0704cb63694e0e946851ca6a7c927cc17e20f
DrKalen <kalen7@gmail.com>
2017-05-26 18:48:34 +0000

    Move action - In Process

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,7 +6,29 @@ class PiecesController < ApplicationController
     @piece.move!(new_x, new_y)
   end
 
+  def move
+    @piece = Piece.find(params[:id])
+    old_x = @piece.x_position
+    old_y = @piece.y_position
+    new_x = params[:x].to_i
+    new_y = params[:y].to_i
+    @piece.move = (shift from old_x, old_y to new_x, new_y)   #how do we represent this, really?
+    if @piece.move.valid? 
+      @piece.update_attributes { :x_position new_x, :y_position new_y }
+      redirect_to game_path(@game)
+    else
+      return render text: 'Move not allowed.', status: :unauthorized
+    end  
+  end
   private
+
+  def current_x
+    #how do we get the computer to know & represent where the piece is after being dragged and dropped?
+  end
+
+  def current_x
+    #how do we get the computer to know & represent where the piece is after being dragged and dropped?
+  end
 
   def piece_params
     params.require(:piece).permit(:x_position, :y_position)
