357d28a532e9af8d0dcc46ade2d7461ce4bf4868
Issei Horie <horie7188@gmail.com>
2017-05-31 02:16:24 +0900

    Add factories for Knight

--- spec/factories/knights.rb
+++ spec/factories/knights.rb
@@ -5,4 +5,47 @@ FactoryGirl.define do
     black false
     association :game, factory: :game
   end
+
+  factory :white_knight_left, class: Knight do
+    type "knight"
+    white true
+    black false
+    x_position 1
+    y_position 7
+    association :game, factory: :game
+  end
+
+  factory :white_knight_right, class: Knight do
+    type "knight"
+    white true
+    black false
+    x_position 6
+    y_position 7
+    association :game, factory: :game
+  end
+
+  factory :black_knight, class: Knight do
+    type "knight"
+    white false
+    black true
+    association :game, factory: :game
+  end
+
+  factory :black_knight_left, class: Knight do
+    type "knight"
+    white false
+    black true
+    x_position 1
+    y_position 0
+    association :game, factory: :game
+  end
+
+  factory :black_knight_right, class: Knight do
+    type "knight"
+    white false
+    black true
+    x_position 6
+    y_position 0
+    association :game, factory: :game
+  end
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Knight, type: :model do 
   it 'should make a move that consists of first one step in a horizontal or vertical direction, and then one step diagonally in an outward direction' do
-    game = FactoryGirl.create(:game)
+    white_knight_left  = FactoryGirl.create(:white_knight_left)
+    white_knight_right = FactoryGirl.create(:white_knight_right)
+    black_knight_left  = FactoryGirl.create(:black_knight_left)
+    black_knight_right = FactoryGirl.create(:black_knight_right)
   end
 end
