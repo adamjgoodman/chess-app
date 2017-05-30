3a2cf619c09e9634b5044cefd6a0a498509114d6
Issei Horie <horie7188@gmail.com>
2017-05-31 02:00:37 +0900

    Add factory game

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Knight, type: :model do 
   it 'should make a move that consists of first one step in a horizontal or vertical direction, and then one step diagonally in an outward direction' do
+    game = FactoryGirl.create(:game)
   end
 end
