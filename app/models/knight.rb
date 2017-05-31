24b98077a23f7904165e8eefaf0d748190c67537
Issei Horie <horie7188@gmail.com>
2017-05-31 20:38:35 +0000

    Add tests

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,10 +1,29 @@
 class Knight < Piece
+
   def move_valid?(x, y)
-    if x_position+1 == x && y_position+2 == y
+    if x < 0 || 7 < x || y < 0 || 7 < y
+      return false
+    end
+
+    if space_occupied?(x, y)
+      return false
+    end
+
+    if x_position+1 == x && y_position-2 == y 
       true
+    elsif x_position+2 == x && y_position-1 == y 
+      true 
     elsif x_position+2 == x && y_position+1 == y
       true
+    elsif x_position+1 == x && y_position+2 == y
+      true
     elsif x_position-1 == x && y_position+2 == y
+      true
+    elsif x_position-2 == x && y_position+1 == y
+      true
+    elsif x_position-2 == x && y_position-1 == y 
+      true
+    elsif x_position-1 == x && y_position-2 == y
       true
     else
       false
--- spec/factories/knights.rb
+++ spec/factories/knights.rb
@@ -1,26 +1,37 @@
 FactoryGirl.define do
   factory :knight do 
-    type "knight"
   end
 
   factory :black_knight_left, class: Knight do
-    type "knight"
     black true
     white false
     x_position 1
     y_position 0
   end
 
+  factory :black_knight_right, class: Knight do
+    black true
+    white false
+    x_position 6
+    y_position 0
+  end
+
   factory :white_knight, class: Knight do 
-    type "knight"
     white true
     black false
   end
 
   factory :white_knight_left, class: Knight do 
-    type "knight"
     white true
     black false
+    x_position 1
+    y_position 7
   end
 
+  factory :white_knight_right, class: Knight do
+    white true
+    black false
+    x_position 6
+    y_position 7
+  end
 end
