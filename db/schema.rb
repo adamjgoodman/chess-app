280ecd05f6da1eaf08e3a5f80ed0d1cc73916129
Jorge-FHP <jnandez565@gmail.com>
2017-06-04 19:31:16 +0000

    drag and drop implemented

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 20170531185252) do
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
     t.boolean  "active"
     t.string   "status"
     t.integer  "user_id_black"
