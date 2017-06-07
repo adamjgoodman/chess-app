550436598f895c9a83cf8cfcb003aa511c2d6af8
DrKalen <kalen7@gmail.com>
2017-06-07 15:02:48 +0000

    Saved files

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -69,8 +69,4 @@ class Piece < ApplicationRecord
     return diagonal_obstructed?(x, y) if diagonal_move?(x, y)
     false
   end
-  #checking for opposite color piece
-  def opposite_color
-    self.is_black == true ? 'white' : 'black'
-  end
 end
