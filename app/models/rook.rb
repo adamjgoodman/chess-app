216f27c0846011a375c37701a883319a0cfc480f
Jacob Gillespie <jacobwgillespie@gmail.com>
2017-06-14 18:43:08 -0500

    Fix rook move_valid? method signature

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,5 +1,5 @@
 class Rook < Piece
-  def move_valid?
+  def move_valid?(x, y)
     return false unless vertical_move?(x, y) || horizontal_move?(x, y)
     return false if path_obstructed?(x, y)
     return false if friendly_piece_at?(x, y)
