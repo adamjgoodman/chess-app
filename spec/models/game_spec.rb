5f6b17aaf12bb82f5bc0fde4fc109a4c2d266a22
PunkLeia <theatrejawa>
2017-06-19 18:32:21 +0000

    fixed failing game_spec test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Game, type: :model do
   it 'should determine that the game is in check' do
     game = Game.create(status: 'available')
     King.active.create(is_black: true, x_position: 3, y_position: 3, game_id: game.id)
-    King.active.create(is_black: false, x_position: 7, y_position: 3)
+    King.active.create(is_black: false, x_position: 7, y_position: 3, game_id: game.id)
     Bishop.active.create(is_black: false, x_position: 5, y_position: 5, game_id: game.id)
     expect(game.black_in_check?).to eq true
     expect(game.game_in_check?).to eq true
@@ -24,6 +24,7 @@ RSpec.describe Game, type: :model do
     Bishop.active.create(is_black: true, x_position: 4, y_position: 2, game_id: game.id)
     King.active.create(is_black: false, x_position: 5, y_position: 3, game_id: game.id)
     expect(game.black_in_check?).to eq false
+    expect(game.white_in_check?).to eq true
     expect(game.game_in_check?).to eq true
   end
 
