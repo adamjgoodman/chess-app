b0e2e249e4f9488efd57761dfb50abf0a62a8280
tynahuynh <tynahuynh@gmail.com>
2017-06-19 10:20:50 -0700

    ajax request

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,9 +1,13 @@
 class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
-    new_x = params[:x].to_i
-    new_y = params[:y].to_i
-    @piece.move!(new_x, new_y)
+    new_x = params[:x_position].to_i
+    new_y = params[:y_position].to_i
+    if @piece.move!(new_x, new_y)
+     return render json: 'success'
+    else
+     render json: 'failure'
+     end
   end
 
   private
