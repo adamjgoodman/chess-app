29c8ed043095c68b6432226f912773ebc319332d
PunkLeia <theatrejawa>
2017-06-20 15:16:30 +0000

    working on turn testing

--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Move, type: :model do
-  # test goes here
+  it "should determine that it is player black's turn" do
+    game = Game.create(status: 'available', user_id_black: 12, user_id_white: 13)
+    game.moves.count == 31
+    expect(game.moves.turn).to eq 12
+  end
 end
