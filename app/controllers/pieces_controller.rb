dd4ebccb3e9527e3ffe80db95ba87445fcc23349
tynahuynh <tynahuynh@gmail.com>
2017-06-19 17:42:43 -0700

    invalid in progress

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,9 +1,24 @@
 class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
+    @game = Game.find(@piece.game_id)
     new_x = params[:x_position].to_i
     new_y = params[:y_position].to_i
-    render json: { error: 0, success: 1 } if @piece.move!(new_x, new_y)
+
+    if @game.user_id_black == @piece.user_id && @piece.is_black
+        @piece.move!(new_x, new_y)
+        render :json => { :error => 0, :success => 1 }
+    end
+
+    if @game.user_id_white == @piece.user_id && !@piece.is_black
+        @piece.move!(new_x, new_y)
+        render :json => { :error => 0, :success => 1 }
+    end
+
+    render :json => { :error => 1, :success => 0 }
+    
+  end
+
   end
 
   private
@@ -12,3 +27,9 @@ class PiecesController < ApplicationController
     params.require(:piece).permit(:x_position, :y_position)
   end
 end
+
+
+
+
+
+
