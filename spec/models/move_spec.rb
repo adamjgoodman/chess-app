01c46f198d41dab64c83bdf620ff3df6fbdccccf
PunkLeia <theatrejawa>
2017-06-20 16:03:46 +0000

    working on move logic

--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Move, type: :model do
+  it "should determine that it is player white's turn" do
+    game = Game.create(status: 'available', user_id_black: 12, user_id_white: 13)
+    move = Move.create(game_id: game.id)
+    game.moves.count == 9
+    expect(move.turn).to eq 13
+  end
+
   it "should determine that it is player black's turn" do
     game = Game.create(status: 'available', user_id_black: 12, user_id_white: 13)
-    game.moves.count == 31
-    expect(game.moves.turn).to eq 12
+    move = Move.create(game_id: game.id)
+    game.moves.count == 8
+    expect(move.turn).to eq 12
   end
 end
