d931c338a80a7f01fa70c1da551d53f440368e83
PunkLeia <theatrejawa>
2017-06-21 22:49:19 +0000

    pushing turn logic as it stands

--- app/models/move.rb
+++ app/models/move.rb
@@ -2,8 +2,6 @@ class Move < ApplicationRecord
   belongs_to :game
   belongs_to :piece
 
-  # count the number of moves (count (like length and size)
-  # is not a 0 index)
   # If even, black is up.  If odd, white.
   def turn
     return game.user_id_black if (game.moves.count).even?
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Move, type: :model do
   it "should determine that player white is up" do
-    game = Game.create(active: true, user_id_black: 110, user_id_white: 111)
+    FactoryGirl.create(:active_game)
     move1 = Move.create(game_id: game.id)
     move2 = Move.create(game_id: game.id)
     move3 = Move.create(game_id: game.id)
@@ -10,7 +10,7 @@ RSpec.describe Move, type: :model do
   end
 
   it "should determine that player black is up" do
-    game = Game.create(active: true, user_id_black: 110, user_id_white: 111)
+    game = Game.create(active: true, user_id_black: 110, user_id_white: 111, index_moves_on_game_id: )
     move1 = Move.create(game_id: game.id)
     move2 = Move.create(game_id: game.id)
     move3 = Move.create(game_id: game.id)
