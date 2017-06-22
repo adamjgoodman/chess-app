e6a5f8010a057d224df4ff5aa8dfb780d5824b0a
PunkLeia <theatrejawa>
2017-06-22 00:30:35 +0000

    added turn validation

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,14 +1,20 @@
 class PiecesController < ApplicationController
+
   def update
-    @piece = Piece.find(params[:id])
-    new_x = params[:x_position].to_i
-    new_y = params[:y_position].to_i
+    @game = Game.find(params[:game_id])
+    if @game.turn == @game.current_user.id
+      @piece = Piece.find(params[:id])
+      new_x = params[:x_position].to_i
+      new_y = params[:y_position].to_i
 
-    response = @piece.move!(new_x, new_y)
+      response = @piece.move!(new_x, new_y)
 
-    flash[:error] = 'Invalid moves!' unless response.nil?
+      flash[:error] = 'Invalid moves!' unless response.nil?
 
-    render json: { error: 0, success: 1 }
+      render json: { error: 0, success: 1 }
+    else
+      flash[:error] = 'Not your move!'
+    end
   end
 
   private
