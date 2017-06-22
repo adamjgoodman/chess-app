f9672267eaadce9320834ae525a579c724d6fc1f
PunkLeia <theatrejawa>
2017-06-22 00:05:12 +0000

    moved turn logic to game model

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,10 +1,4 @@
 class Move < ApplicationRecord
   belongs_to :game
   belongs_to :piece
-
-  # If even, black is up.  If odd, white.
-  def turn
-    return game.user_id_white if game.moves.count.even?
-    game.user_id_black
-  end
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Move, type: :model do
-  it 'should determine that player black is up' do
-    game = Game.create(active: true, user_id_black: 110, user_id_white: 111)
-    king = King.create(game_id: game.id)
-    move1 = Move.create(game_id: game.id, piece_id: king.id)
-    move2 = Move.create(game_id: game.id, piece_id: king.id)
-    move3 = Move.create(game_id: game.id, piece_id: king.id)
-    expect(move3.turn).to eq 110
-  end
-
-  it 'should determine that player white is up' do
-    game = Game.create(active: true, user_id_black: 110, user_id_white: 111)
-    king = King.create(game_id: game.id)
-    move1 = Move.create(game_id: game.id, piece_id: king.id)
-    move2 = Move.create(game_id: game.id, piece_id: king.id)
-    move3 = Move.create(game_id: game.id, piece_id: king.id)
-    move4 = Move.create(game_id: game.id, piece_id: king.id)
-    expect(move4.turn).to eq 111
-  end
+  # test?
 end
